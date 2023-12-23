95f07f58b8af4e1cbd2d3628735b29f11866864b
samsja <sami.jaghouar@gmail.com>
2023-12-23 18:17:05 +0100

    add config manamgnet i
    Signed-off-by: samsja <sami.jaghouar@gmail.com>

--- reason_net/data.py
+++ reason_net/data.py
@@ -1,7 +1,8 @@
 import random
 
-# import torch
-# from torch.utils.data import DataLoader, Dataset
+import torch
+import lightning as L
+from torch.utils.data import Dataset, random_split
 
 
 class MathDataGen:
@@ -46,3 +47,50 @@ class MathTokenizer:
     def decode(self, x: list[int]) -> str:
         decoded = list(map(lambda x: self.anti_vocab[x], list(x)))
         return "".join(decoded)
+
+
+class ListDataset(Dataset):
+    def __init__(self, data):
+        self.data = data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+
+class MathDataModule(L.LightningDataModule):
+    def __init__(self, min: int, max: int, size: int, seed: int):
+        self.size = size
+
+        self.min = min
+        self.max = max
+        self.seed = seed
+
+    def prepare_data(self):
+        self.generator = MathDataGen(min, max)
+        self.tokenizer = MathTokenizer(MathDataGen.operand)
+
+    def setup(self, stage: str):
+        # Assign train/val datasets for use in dataloaders
+        match stage:
+            case "fit":
+                full_raw = [self.generator.generate() for _ in range(self.size)]
+                full_tokenized = [
+                    (self.tokenizer.encode(data), self.tokenizer.encode(target))
+                    for data, target in full_raw
+                ]
+                full_dataset = ListDataset(full_tokenized)
+
+                train_size = int(0.8 * len(full_dataset))
+                val_size = len(full_dataset) - train_size
+
+                self.train, self.val = random_split(
+                    full_dataset,
+                    [train_size, val_size],
+                    generator=torch.Generator().manual_seed(self.seed),
+                )
+
+            case _:
+                raise NotImplementedError()
--- reason_net/run.py
+++ reason_net/run.py
@@ -1,18 +1,27 @@
-from reason_net.data import MathDataGen, MathTokenizer
+from typing import Any, cast
+import hydra
+from omegaconf import DictConfig, OmegaConf
+from reason_net.data import MathDataModule
+from pydantic import BaseModel
 
 
-def run():
-    generator = MathDataGen(0, 3)
-    tokenizer = MathTokenizer(MathDataGen.operand)
+class RunConfig(BaseModel):
+    seed: int
 
-    for _ in range(10):
-        data, target = generator.generate()
-        print(data, target)
 
-        encode = tokenizer.encode(data)
-        print(encode)
-        print(tokenizer.decode(encode))
+def run(conf: RunConfig):
+    _data = MathDataModule(1, 2, 1000, conf.seed)
+
+    print("here")
+    print(conf.seed)
+
+
+@hydra.main(version_base="1.2")
+def main(raw_conf: DictConfig):
+    dict_conf = cast(dict[str, Any], OmegaConf.to_container(raw_conf))
+    conf = RunConfig(**dict_conf)
+    run(conf)
 
 
 if __name__ == "__main__":
-    run()
+    main()
