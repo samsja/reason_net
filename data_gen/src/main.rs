4aabea04256771b567aa20f4daef7b6ede2d83d0
samsja <sami.jaghouar@gmail.com>
2024-01-07 16:26:16 +0100

    add exo generator
    Signed-off-by: samsja <sami.jaghouar@gmail.com>

--- data_gen/src/main.rs
+++ data_gen/src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use rand::{Rng, SeedableRng};
+use rand::rngs::StdRng;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -6,23 +8,57 @@ use clap::Parser;
 struct Args {
 
     #[arg(long)]
-    min: u8,
+    min: u32,
 
     #[arg(long)]
-    max: u8,
+    max: u32,
 
     #[arg(long)]
-    size: u8,
+    size: u32,
 
     #[arg(long)]
-    seed: u8,
+    seed: u64,
 
     #[arg(long)]
     save_file_path: String,
 
 }
 
+
+fn generate_datapoint(min: u32, max: u32, rng: &mut StdRng) -> String {
+
+    let min_val = 10i64.pow(min);
+    let max_val = 10i64.pow(max);
+
+    let a: i64 = rng.gen_range(min_val..max_val);
+    let b: i64 = rng.gen_range(min_val..max_val);
+
+    let operand = rng.gen_range(0..5);
+
+    let exo = match operand {
+        0 => format!("{a}+{b}={}", a+b),
+        1 => format!("{a}-{b}={}", a-b),
+        2 => format!("{a}*{b}={}", a*b),
+        3 => format!("{a}/{b}={}", a/b),
+        4 => format!("{a}%{b}={}", a%b),
+        _ => panic!("Invalid operand"),
+    };
+
+    exo
+
+}
+
+fn generate_all(conf: Args) {
+    let mut rng = StdRng::seed_from_u64(conf.seed);
+
+    for _ in 0..conf.size {
+        let datapoint = generate_datapoint(conf.min, conf.max, &mut rng);
+        println!("{}", datapoint);
+    }
+}
+
 fn main() {
     let args = Args::parse();
     println!("{:?}", args);
+    generate_all(args);
 }
