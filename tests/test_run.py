fe0d3421c78d1bce3cdca287c7adfa361114dccc
samsja <sami.jaghouar@gmail.com>
2024-02-14 11:44:25 +0100

    refactor test

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,24 +1,19 @@
 from pathlib import Path
-from omegaconf import DictConfig, open_dict
+from omegaconf import open_dict
 import pytest
 import torch
 
-from reason_net.run import run, omegaconf_to_pydantic
+from reason_net.run import RunConfig, run, omegaconf_to_pydantic
 from reason_net.generate import generate
 from hydra import compose, initialize
 
 
-def raw_config() -> DictConfig:
+def _init_config() -> RunConfig:
     with initialize(
         version_base=None,
         config_path="../reason_net/configs",
     ):
-        raw_conf = compose(config_name="all-14m.yaml", overrides=["module/model=910K"])
-
-    return raw_conf
-
-
-def _init_config(cfg: DictConfig) -> DictConfig:
+        cfg = compose(config_name="all-14m.yaml", overrides=["module/model=910K"])
     with open_dict(cfg):
         cfg.trainer.pl.max_epochs = 2
         cfg.data.num_workers = 0
@@ -29,39 +24,39 @@ def _init_config(cfg: DictConfig) -> DictConfig:
             "norm_monitor": {"log_every_n_steps": 1},
         }
 
-    return cfg
+    return omegaconf_to_pydantic(cfg)
 
 
-def normal_config() -> DictConfig:
-    raw_conf = _init_config(raw_config())
-    config = omegaconf_to_pydantic(raw_conf)
+def normal_config() -> RunConfig:
+    config = _init_config()
 
     config.reason_mode = False
+
     return config
 
 
-def reason_middle_config() -> DictConfig:
-    raw_conf = _init_config(raw_config())
-    config = omegaconf_to_pydantic(raw_conf)
+def reason_middle_config() -> RunConfig:
+    config = _init_config()
 
     config.reason_mode = True
     config.data.reason.reason_token_pos = "middle"  # type: ignore
+
     return config
 
 
-def reason_left_config() -> DictConfig:
-    raw_conf = _init_config(raw_config())
-    config = omegaconf_to_pydantic(raw_conf)
+def reason_left_config() -> RunConfig:
+    config = _init_config()
 
     config.reason_mode = True
     config.data.reason.reason_token_pos = "left"  # type: ignore
+
     return config
 
 
 @pytest.mark.parametrize(
     "config", [normal_config(), reason_middle_config(), reason_left_config()]
 )
-def test_run(config: DictConfig, tmp_path: Path):
+def test_run(config: RunConfig, tmp_path: Path):
     config.trainer.save_dir = tmp_path
 
     module, data = run(config)
