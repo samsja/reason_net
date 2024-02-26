06da416d49d0be41fc38bdd36b9bded82d280fd4
Sami Jaghouar <sami.jaghouar@hotmail.fr>
2024-02-26 19:24:52 +0000

    come back to simple data loading

--- reason_net/data.py
+++ reason_net/data.py
@@ -3,16 +3,16 @@ from abc import ABC, abstractmethod
 from collections import defaultdict
 import os
 from pathlib import Path
-from typing import ClassVar, Generator, Literal, TypeAlias, TypeVar
+from typing import ClassVar, Literal, TypeAlias, TypeVar
 import typing
-from itertools import islice
 
+import numpy as np
 
 from reason_net.pydantic_conf import Config
 import torch
 from torch import Tensor
 import lightning as L
-from torch.utils.data import IterableDataset, DataLoader
+from torch.utils.data import Dataset, DataLoader
 from jaxtyping import Int, jaxtyped
 from beartype import beartype as typechecker
 
@@ -96,25 +96,7 @@ b = TypeVar("b")
 DatasetOutput: TypeAlias = tuple[list[int], dict[str, int]]
 
 
-def split_by_worker(
-    source: Generator[DatasetOutput, None, None],
-) -> Generator[DatasetOutput, None, None]:
-    winfo = torch.utils.data.get_worker_info()
-    if winfo is None:
-        yield from source
-    else:
-        yield from islice(source, winfo.id, None, winfo.num_workers)
-
-
-def split_by_rank(
-    source: Generator[DatasetOutput, None, None],
-    rank: int,
-    world_size: int,
-) -> Generator[DatasetOutput, None, None]:
-    yield from islice(source, rank, None, world_size)
-
-
-class BaseMathDataset(IterableDataset, ABC):
+class BaseMathDataset(Dataset, ABC):
     """
     This just load a file split by the equal sign and a define simple
     padded data collator
@@ -142,40 +124,18 @@ class BaseMathDataset(IterableDataset, ABC):
 
         self.chunks_files.sort()
 
-        with open(self.dataset_path / self.chunks_files[0], "r") as f:
-            self.chunk_size = len([line.strip() for line in f])
-
-        self.current_file: str | None = None
-        self.current_data: list[str]
-
-        with open(self.dataset_path / self.chunks_files[-1], "r") as f:
-            self.last_chunk_size = len([line.strip() for line in f])
-
-    def __len__(self) -> int:
-        winfo = torch.utils.data.get_worker_info()
-        if winfo is None:
-            return (len(self.chunks_files) - 1) * self.chunk_size + self.last_chunk_size
-        else:
-            return (
-                (len(self.chunks_files) - 1) * self.chunk_size + self.last_chunk_size
-            ) // winfo.num_workers
-            # todo check here if the division is correct
-
-    def _raw_iter(self) -> Generator[DatasetOutput, None, None]:
+        data = []
         for chunk_file in self.chunks_files:
             with open(self.dataset_path / chunk_file, "r") as f:
-                for line in f:
-                    data_point = line.strip()
-                    yield self.preprocess_data_point(data_point)
+                data.extend([line.strip() for line in f])
 
-    def __iter__(self) -> Generator[DatasetOutput, None, None]:
-        if torch.distributed.is_available() and torch.distributed.is_initialized():
-            group = torch.distributed.group.WORLD
-            rank = torch.distributed.get_rank(group=group)
-            size = torch.distributed.get_world_size(group=group)
-            return split_by_worker(split_by_rank(self._raw_iter(), rank, size))
-        else:
-            return split_by_worker(self._raw_iter())
+        self.data = np.array(data)
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __getitem__(self, idx: int) -> DatasetOutput:
+        return self.preprocess_data_point(self.data[idx])
 
     def split_data_point(self, data_point: str) -> tuple[list[int], list[int]]:
         [left, right] = data_point.split("=")
